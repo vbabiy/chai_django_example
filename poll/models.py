b6975a0f2272397e47ed0b2f9a1ba8c5f0bd97a3
Vitaly Babiy <vbabiy86@gmail.com>
2011-06-05 20:42:10 -0400

    Simple model and view, testing using chai

--- poll/models.py
+++ poll/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 class Poll(models.Model):
     name = models.CharField(max_length=255)
     visable = models.BooleanField(default=False)
     publish_date = models.DateTimeField()
 
-    def is_vislable(self):
+    def is_visable(self):
+        if self.publish_date < datetime.now():
+            return False
         return True
 
 
--- poll/tests.py
+++ poll/tests.py
@@ -1,13 +1,8 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from chai import Chai
 from models import Poll
+import models
+import datetime
 
 
 class PollShowTestCase(Chai, TestCase):
@@ -28,6 +23,29 @@ class PollShowTestCase(Chai, TestCase):
         res = self.client.get("/poll/show/1/")
         self.assert_equals(res.status_code, 302)
 
+class PollModelTestCase(Chai, TestCase):
+
+    def setUp(self):
+        super(PollModelTestCase, self).setUp()
+        self.subject = Poll()
+    
+    def test_is_visable(self):
+        now = datetime.datetime.now()
+
+        dt = self.mock()
+        mock_dt = self.mock(models, 'datetime')
+        self.expect(mock_dt.now).returns(now)
+        self.subject.publish_date = now
+        assert self.subject.is_visable()
+    
+    def test_is_not_visable(self):
+        now = datetime.datetime.now()
+
+        dt = self.mock()
+        mock_dt = self.mock(models, 'datetime')
+        self.expect(mock_dt.now).returns(now)
+        self.subject.publish_date =  now - datetime.timedelta(days=10)
+        assert not self.subject.is_visable()
 
 
 
